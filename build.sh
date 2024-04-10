938ebf521c88196162e56ba70c31d515dd1f0ea7
1z1gh0st <evelyn@gh0st.lv>
2024-04-09 23:12:25 -0700

    GitIgnore created.

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # Run the Python script to flatten cart
 python3 flatten_cart.py moonquest.p8 moonquest_compact.p8
 
+# Delete build folder and remake it
+rm -rf build/
+mkdir build
+
 # Move the compacted cart to the build directory
 mv moonquest_compact.p8 build/
 
