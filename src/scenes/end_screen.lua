6cc461a4c83be175647d2f88becf5799eac9964a
gh0st <evelyn@gh0st.lv>
2024-12-29 00:26:59 -0800

    Update end_screen.lua

--- src/scenes/end_screen.lua
+++ src/scenes/end_screen.lua
@@ -1,6 +1,5 @@
 function end_screen_init()
     music(46)
-    controls_on = false
     if num_deaths == 0 then
         end_screen.text = '100 moons found...\nand still with your flesh intact...\nyou impress me deeply.'
     else
@@ -16,13 +15,12 @@ function end_screen.update()
     end
     if btnp(4) then
         end_screen.hide = true
-        controls_on = true
     end
 end
 
 function end_screen.draw()
     if not end_screen.hide then
         local substring=sub(end_screen.text,0,min(flr(end_screen.index),#end_screen.text))
-        draw_rounded_textbox(cam.x, cam.y, substring)
+        draw_rounded_textbox(player.x, player.y, substring)
     end
 end
